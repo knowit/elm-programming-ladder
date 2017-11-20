144189f06e0392d75bca0564140ba190913ab646
Tale Prestmo <tale.prestmo@knowit.no>
2017-11-20 20:58:17 +0100

    Working initial app

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,17 +1,24 @@
 module Main exposing (..)
-import Html exposing (..)
 
-import Components.Hello exposing ( hello )
+import Html exposing (..)
+import Components.Hello exposing (hello)
 import Models exposing (Model, initialModel)
 import Updates exposing (update, Msg)
 import View exposing (view)
 
+
 -- INIT
-init: (Model, Cmd Msg)
+
+
+init : ( Model, Cmd Msg )
 init =
-  (initialModel, Cmd.None)
+    ( initialModel, Cmd.none )
+
+
 
 -- APP
+
+
 main : Program Never Model Msg
 main =
-  Html.beginnerProgram { init = init, model = model, view = view, update = update }
+    Html.beginnerProgram { model = initialModel, view = view, update = update }
--- src/elm/View.elm
+++ src/elm/View.elm
@@ -1,17 +1,22 @@
 module View exposing (..)
+
 import Models exposing (Model)
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing ( onClick )
+import Html.Events exposing (onClick)
 import Updates exposing (update, Msg)
+
 
 view : Model -> Html Msg
 view model =
-  div [    -- inline CSS (literal)
-    div [ class "navigation" ][    -- inline CSS (literal)
-      span[][ text "Nav" ]
-    ],
-    div [ class "container" ][    -- inline CSS (literal)
-      span[][ text "container" ]
-    ]
-  ]
+    div []
+        [ -- inline CSS (literal)
+          div [ class "navigation" ]
+            [ -- inline CSS (literal)
+              span [] [ text "Nav" ]
+            ]
+        , div [ class "container" ]
+            [ -- inline CSS (literal)
+              span [] [ text "container" ]
+            ]
+        ]
