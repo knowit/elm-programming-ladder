4e8fa27ae0f12da982f9b5014b2fb3ab32bc941a
Pernille Johnsen <pernillej11@gmail.com>
2019-01-31 15:53:12 +0100

    Displays table with temp hardcoded users

--- src/Page/Stats.elm
+++ src/Page/Stats.elm
@@ -1,1 +1,40 @@
 module Page.Stats exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+main =
+    div [ class "content-main" ]
+        [ h1 [] [ text "Ledertavle" ]
+        , table [] <|
+            [tr []
+                [ th [] [ text "Plass" ]
+                , th [] [ text "Bruker"]
+                ]
+            ]
+            ++ (List.map viewUser users)
+        ]
+                
+
+viewUser: User -> Html msg
+viewUser user = 
+    tr []
+        [ th [] [ text (String.fromInt user.score) ]
+        , th [] [ text user.name ]
+        ]
+
+-- Users (Temp)
+
+type alias User = 
+    { name: String
+    , score: Int
+    }
+
+users: List User
+users =
+    [ User "User 1" 1
+    , User "User 2" 2
+    , User "User 3" 3
+    , User "User 4" 4
+    , User "User 5" 5
+    ]
