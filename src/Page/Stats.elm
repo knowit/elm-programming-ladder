e5ca3ede2783e88b3a7342d37756d4b7ef318709
Pernille Johnsen <pernillej11@gmail.com>
2019-02-07 12:46:15 +0100

    Added table grouping views.

--- src/Page/Stats.elm
+++ src/Page/Stats.elm
@@ -6,15 +6,26 @@ import Html.Attributes exposing (..)
 main =
     div [ class "content-main" ]
         [ h1 [] [ text "Ledertavle" ]
-        , table [] <|
-            [tr []
+        , table []
+            ([ thead []
                 [ th [] [ text "Plass" ]
                 , th [] [ text "Bruker"]
                 ]
             ]
-            ++ (List.map viewUser users)
+                ++ List.map viewUserGroup userGroups
+            )
         ]
+        
                 
+viewUserGroup: UserGroup -> Html msg
+viewUserGroup userGroup =
+    tbody [] 
+        ([ tr []
+            [ th [] [text (String.fromInt userGroup.score ++ " luker")] ] 
+            ]
+        ++ List.map viewUser userGroup.users
+        )
+    
 
 viewUser: User -> Html msg
 viewUser user = 
@@ -22,19 +33,39 @@ viewUser user =
         [ th [] [ text (String.fromInt user.score) ]
         , th [] [ text user.name ]
         ]
+    
 
 -- Users (Temp)
+
+type alias UserGroup = 
+    { score: Int
+    , users: List User
+    }
+
+userGroups: List UserGroup
+userGroups = 
+    [ UserGroup 24 users1 
+    , UserGroup 23 users2
+    ]
 
 type alias User = 
     { name: String
     , score: Int
     }
 
-users: List User
-users =
+users1: List User
+users1 =
     [ User "User 1" 1
-    , User "User 2" 2
-    , User "User 3" 3
-    , User "User 4" 4
-    , User "User 5" 5
+    , User "User 2" 1
+    , User "User 3" 1
+    , User "User 4" 1
+    , User "User 5" 1
+    ]
+
+users2: List User
+users2 =
+    [ User "User 6" 2
+    , User "User 7" 2
+    , User "User 8" 2
+    , User "User 9" 2
     ]
